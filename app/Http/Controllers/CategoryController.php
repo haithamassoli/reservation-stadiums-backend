939651fde30fd0640d5b9ca3a23766c6b4b2ed69
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-29 13:14:26 +0300

    added categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json($categories);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
         $this->call([
             CitySeeder::class,
+            CategorySeeder::class,
         ]);
 
         // User::factory()->create([
