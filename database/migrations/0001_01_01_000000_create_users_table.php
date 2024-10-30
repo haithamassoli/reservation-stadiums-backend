c82e40cccd4eb54e26dd35a5f774ceb6badf5fbb
haithamassoli <haitham.b.assoli@gmail.com>
2024-10-30 06:49:07 +0300

    Add phone and violations fields to User model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->integer('violations')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
