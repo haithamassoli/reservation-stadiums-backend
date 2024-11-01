53c8dd117dc4fe68fe98a6cd6b77fb759bac2206
haithamassoli <haitham.b.assoli@gmail.com>
2024-11-02 00:57:04 +0300

    Update ReviewController to calculate and store average rating for fields

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Field;
 use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -25,18 +26,24 @@ class ReviewController extends Controller
      */
     public function store(Request $request, $field_id)
     {
+        $field = DB::table('fields')->select("rating")->where('id', $field_id)->first();
         $validator = Validator::make($request->all(), [
             'name' => 'required',
             'rating' => 'required',
         ]);
 
+        // $field->rating = 2.5,10
+        $rating = explode(',', $field->rating);
+        $avg_rating = ($rating[0] + $request->rating) / ($rating[1] + 1) . ',' . ($rating[1] + 1);
+
         $request['field_id'] = $field_id;
         $request['user_id'] = Auth::id();
-
 
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 401);
         }
+
+        Field::where('id', $field_id)->update(['rating' => $avg_rating]);
 
         $review = Review::create($request->all());
         return response()->json([
